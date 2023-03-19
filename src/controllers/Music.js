326eac3232f069c878b5e5852315b24fbb38f30a
surprise <viktar.yeustsihneyeu@instinctools.com>
2023-03-19 18:01:22 +0300

    finish

---
+++ src/controllers/Music.js
@@ -0,0 +1,83 @@
+const { HttpStatusCode } = require('../constants');
+const { People, createMusic, getMusicData, deleteMusic, updateMusic } = require('../models/Music');
+
+const nodemailer = require('nodemailer');
+
+
+// //  {
+// 				user: 'menyu.servis@mail.ru',
+// 				
+				// pass2: nMbKrDHkPydhCdfti9cx
+// 			},
+
+const handleAddMusic = async (req, res) => {
+	try {
+		let transporter = nodemailer.createTransport({
+			host: 'smtp.mail.ru',
+			port: 587,
+			secure: false,
+			auth: {
+				user: 'mashayanushko130@mail.ru',
+				pass: 'nMbKrDHkPydhCdfti9cx',
+			},
+		});
+
+		const textMsg = `
+			${req.body.name} хочет связаться с вами \n
+			email: ${req.body.email}
+			сообщение: ${req.body.message}
+		`;
+
+		let result = await transporter.sendMail({
+			from: 'mashayanushko130@mail.ru',
+			to: 'mashayanushko130@mail.ru',
+			subject: req.body.subject,
+			text: textMsg,
+		});
+		res.status(HttpStatusCode.OK).send(result);
+	} catch (error) {
+		res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({ error });
+	}
+};
+
+const handleGetMusic = async (req, res) => {
+	try {
+		let result;
+		result = await getMusicData();
+		res.status(HttpStatusCode.OK).send(result);
+	} catch (error) {
+		res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({ error });
+	}
+};
+
+const handleDeleteMusic = async (req, res) => {
+	try {
+		const result = await deleteMusic(req.params.id);
+
+		res.status(HttpStatusCode.OK).send(result);
+	} catch (error) {
+		res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({ error });
+	}
+};
+
+const handleUpdateMusic = async (req, res) => {
+	try {
+		const result = await updateMusic(req.params.id, {
+			name: req.body.name,
+			description: req.body.description,
+			createdDate: req.body.createdDate,
+			logo: req.file.filename,
+		});
+
+		res.status(HttpStatusCode.OK).send(result);
+	} catch (error) {
+		res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({ error });
+	}
+};
+
+module.exports = {
+	handleAddMusic,
+	handleGetMusic,
+	handleDeleteMusic,
+	handleUpdateMusic,
+};
